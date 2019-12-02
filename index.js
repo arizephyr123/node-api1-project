2f00c98f618df6fe7176041d645d51ee95887ece
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-02 13:24:37 -0800

    Initial Commit: server set up in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const port = ('5555');
+
+const server = express();
+
+server.get('/', (req, res) => {
+res.send("Hello World from Express")
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
